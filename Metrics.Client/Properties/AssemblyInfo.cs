d2199b0ea07fb7d105836de4b9f5cdd5974df0b2
Joe Biellik <joebiellik@gmail.com>
2018-12-29 20:09:09 +0000

    Cleanup

--- Metrics.Client/Properties/AssemblyInfo.cs
+++ Metrics.Client/Properties/AssemblyInfo.cs
@@ -2,7 +2,7 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 using NFive.SDK.Client;
 
-[assembly: ClientPlugin(1)]
+[assembly: ClientPlugin(SDK.Version)]
 
 [assembly: AssemblyTitle("NFive Metrics")]
 [assembly: AssemblyDescription("Metric logging with Elasticsearch")]
